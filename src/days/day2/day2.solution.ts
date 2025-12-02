61e586bcdaecd58093aa3e8a02c5587786053607
romanurban <roman.urban.orban@gmail.com>
2025-12-02 11:12:28 +0200

    day 2 solution

--- src/days/day2/day2.solution.ts
+++ src/days/day2/day2.solution.ts
@@ -11,7 +11,25 @@ export class Day2Solution implements DaySolution {
       return 'Provide puzzle input in inputs/day2.txt or via --input/--raw flags.';
     }
 
-    return 'Part one not implemented yet.';
+    let matchSum = 0;
+    let lines = input.split(',').map(line => line.trim()).filter(line => line.length > 0);
+
+    for (let line of lines) {
+      let rangeStart = line.split('-')[0];
+      let rangeEnd = line.split('-')[1];
+
+      for (let i = parseInt(rangeStart, 10); i <= parseInt(rangeEnd, 10); i++) {
+        let currNum = i.toString();
+        if (this.isEven(currNum.length)) {
+          if (currNum.slice(0, currNum.length / 2) === currNum.slice(currNum.length / 2)) { // left and right halves of number match
+            console.log(`Matching number found: ${i}`);
+            matchSum += i;
+          }
+        }
+      }
+    }
+
+    return `Sum of all matching numbers: ${matchSum}`;
   }
 
   partTwo(input: string): DayResult {
@@ -19,6 +37,34 @@ export class Day2Solution implements DaySolution {
       return 'Provide puzzle input in inputs/day2.txt or via --input/--raw flags.';
     }
 
-    return 'Part two not implemented yet.';
+
+    let matchSum = 0;
+    let lines = input.split(',').map(line => line.trim()).filter(line => line.length > 0);
+
+    for (let line of lines) {
+      let rangeStart = line.split('-')[0];
+      let rangeEnd = line.split('-')[1];
+
+      for (let i = parseInt(rangeStart, 10); i <= parseInt(rangeEnd, 10); i++) {
+        let currNum = i.toString();
+        const n = currNum.length;
+        for ( let j = 1; j <= Math.floor(n / 2); j++ ) {
+          const supposedPattern = currNum.slice(0, j);
+          const repeatedPattern = supposedPattern.repeat(n / j);
+          if ( repeatedPattern === currNum ) {
+            console.log(`Matching number found: ${j}`);
+            matchSum += i;
+            break;
+          }
+        }
+
+      }
+    }
+
+    return `Sum of all matching numbers: ${matchSum}`;
   }
+
+  isEven(n) {
+    return n % 2 == 0;
+    }
 }

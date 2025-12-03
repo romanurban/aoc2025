b8eadad260a409e96ce8f819e43204d5f40b14a5
romanurban <roman.urban.orban@gmail.com>
2025-12-03 12:38:34 +0200

    day3 part1 solution

--- src/days/day3/day3.solution.ts
+++ src/days/day3/day3.solution.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DayResult, DaySolution } from '../day-solution';
+import { concat } from 'rxjs';
 
 @Injectable()
 export class Day3Solution implements DaySolution {
@@ -11,7 +12,21 @@ export class Day3Solution implements DaySolution {
       return 'Provide puzzle input in inputs/day3.txt or via --input/--raw flags.';
     }
 
-    return 'Part one not implemented yet.';
+    let lines = input.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+    let totalScore = 0;
+    for (let line of lines) {
+      let lineArr = line.split('');
+      let lastArrEl = lineArr.pop()
+      let firstDigit = Math.max(...lineArr.map(c => parseInt(c, 10)));
+      let firstDigitIndex = lineArr.indexOf(firstDigit.toString());
+      let arrAfterFirstDigit = lineArr.slice(firstDigitIndex + 1);
+      arrAfterFirstDigit.push(lastArrEl!);
+      let secondDigit = Math.max(...arrAfterFirstDigit.map(c => parseInt(c, 10)));
+      console.log(`First digit: ${firstDigit}, Second digit: ${secondDigit}, Product: ${firstDigit * secondDigit}`);
+      totalScore += parseInt(firstDigit.toString().concat(secondDigit.toString())); 
+    }
+
+    return `Total score: ${totalScore}`;
   }
 
   partTwo(input: string): DayResult {
